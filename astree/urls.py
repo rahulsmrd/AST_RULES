3114638433424c12e50ff797277934f2a46a2a7b
rahulsmrd <rahulsmrd@gmail.com>
2024-10-20 07:46:59 +0530

    AST Rules

--- astree/urls.py
+++ astree/urls.py
@@ -4,8 +4,7 @@ from astree.views import combine_rules, evaluate_rule, home, ListAllRules
 app_name = "astree"
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', ListAllRules, name='all_rules'),
     path('create_rule/', combine_rules, name='create_rule'),
     path('evaluate_rule/', evaluate_rule, name='evaluate_rule'),
-    path('all_rules/', ListAllRules, name='all_rules'),
 ]
